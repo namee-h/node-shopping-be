ea6a48ca9da2f077506f0f63596453a4cd9bdef1
Sara han <hans63237@gmail.com>
2025-08-03 02:59:20 +0900

    chore: 선언순서 수정

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const indexRouter = require("./routes/index");
 const app = express();
+
+require("dotenv").config();
+
 const LOCAL_DB_ADDRESS = process.env.LOCAL_DB_ADDRESS;
 const PROD_DB_ADDRESS = process.env.PROD_DB_ADDRESS;
-const PORT = process.env.PORT || 8080;
-const mongoURI = PORT === 8080 ? LOCAL_DB_ADDRESS : PROD_DB_ADDRESS;
-require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+const mongoURI = PORT === 8000 ? LOCAL_DB_ADDRESS : PROD_DB_ADDRESS;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); //req.body 가 객체로 인식됨
