bfc3ae35de192f030d33fffb12332c89408df226
Sara han <hans63237@gmail.com>
2025-08-05 01:17:11 +0900

    feat: product 라우터 추가

---
+++ controllers/product.controller.js
@@ -0,0 +1,32 @@
+const Product = require("../models/Product");
+const productController = {};
+
+productController.createProduct = async (req, res) => {
+  try {
+    const { sku, name, size, category, price, description, image, stock } =
+      req.body;
+    const product = new Product({
+      sku,
+      name,
+      size,
+      category,
+      price,
+      description,
+      image,
+      stock,
+    });
+    await product.save();
+    res.status(200).json({ status: "success", product });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
+};
+productController.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({ status: "success", products });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
+};
+module.exports = productController;
