6db95f2948049589834eb2f79f5e9523a8fde379
Sara han <hans63237@gmail.com>
2025-08-09 18:50:58 +0900

    feat: 카트 아이템 조회하기

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -4,5 +4,6 @@ const cartController = require("../controllers/cart.controller");
 const authController = require("../controllers/auth.controller");
 
 router.post("/", authController.authenticate, cartController.addItemToCart);
+router.get("/", authController.authenticate, cartController.getCart);
 
 module.exports = router;
