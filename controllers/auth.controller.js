e4880b05682e6181b5d9bf56515c29331e31cde9
Sara han <hans63237@gmail.com>
2025-08-17 12:07:59 +0900

    feat: google Login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,10 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const { OAuth2Client } = require("google-auth-library");
 require("dotenv").config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const authController = {};
 
 authController.loginWithEmail = async (req, res) => {
@@ -22,6 +24,34 @@ authController.loginWithEmail = async (req, res) => {
     return res.status(400).json({ status: "fail", error: error.message });
   }
 };
+
+authController.loginWithGoogle = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
+    const ticket = await googleClient.verifyIdToken({
+      idToken: token,
+      audience: GOOGLE_CLIENT_ID,
+    });
+
+    const { email, name } = ticket.getPayload();
+    let user = await User.findOne({ email });
+    if (!user) {
+      const randomPassword = Math.random().toString(36).substring(2, 15);
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(randomPassword, salt);
+      user = new User({ email, name, password: hashedPassword });
+      await user.save();
+    }
+    const sessionToken = await user.generateToken();
+    return res
+      .status(200)
+      .json({ status: "success", user, token: sessionToken });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
 authController.authenticate = async (req, res, next) => {
   try {
     const tokenString = req.headers.authorization;
@@ -37,6 +67,7 @@ authController.authenticate = async (req, res, next) => {
     return res.status(400).json({ status: "fail", error: error.message });
   }
 };
+
 authController.checkAdminPermission = async (req, res, next) => {
   try {
     const { userId } = req;
